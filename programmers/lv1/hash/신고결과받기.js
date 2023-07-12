8ff14f75212820de92f35d02470bb3160507f210
yunmi099 <yunmi099@ajou.ac.kr>
2023-07-12 20:30:56 +0900

    신고결과받기 예제 리팩토링

--- programmers/lv1/hash/신고결과받기.js
+++ programmers/lv1/hash/신고결과받기.js
@@ -1,11 +1,24 @@
+const id_list = ["muzi", "frodo", "apeach", "neo"];
+const report = [
+  "muzi frodo",
+  "apeach frodo",
+  "frodo neo",
+  "muzi neo",
+  "apeach muzi",
+];
+k = 2;
 function solution(id_list, report, k) {
   report = [...new Set(report)];
+  // []=> set => []
   var answer = Array.from({ length: id_list.length }, () => 0);
   let info = [];
   let result = [];
   let stopPeople = [];
   let count = Array.from({ length: id_list.length }, () => 0);
-  report.forEach((el, idx) => info.push(el.split(" ")));
+  //   report.forEach((el, idx) => info.push(el.split(" ")));
+  info = report.map((key) => key.split(" "));
+  console.log(info);
+
   id_list.forEach((el, idx) => {
     info.forEach((key) => {
       if (key[1] === el) {
@@ -16,17 +29,17 @@ function solution(id_list, report, k) {
       }
     });
   });
-  result.map((key, index) => {
+  console.log(result);
+  result.map((key) => {
     if (Object.values(key) >= k) {
       stopPeople.push(Object.keys(key)[0]);
     }
   });
 
-  stopPeople.map((key, index) => {
+  stopPeople.map((key) => {
     info.forEach((el) => {
       el[1] === key ? (answer[id_list.indexOf(el[0])] += 1) : null;
     });
   });
   return answer;
 }
-console.log(solution(id_list, report, k));
